7c16e499ea59093f619884cfbe5559c5d625a3df
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-12-06 10:49:35 +0100

    Add code to handle Loading Drum mover

--- turret/code/loading-drum/motor.h
+++ turret/code/loading-drum/motor.h
@@ -1,52 +1,107 @@
 #ifndef __MOTOR_H__
 #define __MOTOR_H__
 
+#define OPEN_GATE \
+  digitalWrite(GATE_MOTOR_FORWARD_PIN, LOW); \
+  digitalWrite(GATE_MOTOR_BACKWARD_PIN, HIGH)
+
+#define CLOSE_GATE \
+  digitalWrite(GATE_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(GATE_MOTOR_BACKWARD_PIN, LOW)
+  
+#define STOP_GATE \
+  digitalWrite(GATE_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(GATE_MOTOR_BACKWARD_PIN, HIGH)
+
+#define GATE_CLOSED_TRIGGERED analogRead(GATE_CLOSED_END_PIN) > 500
+#define GATE_OPENED_TRIGGERED analogRead(GATE_OPENED_END_PIN) > 500
+
+#define MOVE_LOADING_DRUM_FORWARD \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_BACKWARD_PIN, LOW); \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_FORWARD_PIN, HIGH)
+
+#define MOVE_LOADING_DRUM_BACKWARD \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_BACKWARD_PIN, HIGH); \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_FORWARD_PIN, LOW)
+
+#define STOP_LOADING_DRUM_MOVER \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_BACKWARD_PIN, HIGH); \
+  digitalWrite(LOADING_DRUM_MOVER_MOTOR_FORWARD_PIN, HIGH)
+
 enum State {
   NONE,
   OPEN,
   CLOSE
 };
 
-class ConstrainedMotor {
+class Gate {
 public:
-  ConstrainedMotor(uint8_t motor_forward_pin, uint8_t motor_backward_pin, uint8_t close_end_pin, uint8_t open_end_pin) {
-    _motor_forward_pin = motor_forward_pin;
-    _motor_backward_pin = motor_backward_pin;
-    _close_end_pin = close_end_pin;
-    _open_end_pin = open_end_pin;
-  }
-
   void open() {
-    _open_state = OPEN;
+    _state = OPEN;
   }
 
   void close() {
-    _open_state = CLOSE;
+    _state = CLOSE;
   }
 
   void tick() {
-    if (_open_state == OPEN) {
-      if (analogRead(_open_end_pin) < 500) {
-        digitalWrite(_motor_forward_pin, LOW);
-        digitalWrite(_motor_backward_pin, HIGH);
-      } else {
-        digitalWrite(_motor_forward_pin, HIGH);
-        digitalWrite(_motor_backward_pin, HIGH);
-      }
-    } else if (_open_state == CLOSE) {
-      if (analogRead(_close_end_pin) < 500) {
-        digitalWrite(_motor_forward_pin, HIGH);
-        digitalWrite(_motor_backward_pin, LOW);
-      } else {
-        digitalWrite(_motor_forward_pin, HIGH);
-        digitalWrite(_motor_backward_pin, HIGH);
-      }
+    switch (_state) {
+
+      case OPEN:
+        if (GATE_OPENED_TRIGGERED) {
+          STOP_GATE;
+        } else {
+          OPEN_GATE;
+        }
+        break;
+
+      case CLOSE:
+        if (GATE_CLOSED_TRIGGERED) {
+          STOP_GATE;
+        } else {
+          CLOSE_GATE;
+        }
+        break;
     }
   }
 
 private:
-  uint8_t _motor_forward_pin, _motor_backward_pin, _close_end_pin, _open_end_pin;
-  State _open_state = NONE;
+  State _state = NONE;
+};
+
+class LoadingDrumMover {
+public:
+  void activate() {
+    _state = OPEN;
+  }
+
+  void deactivate() {
+    _state = CLOSE;
+  }
+
+  void tick(void) {
+    switch (_state) {
+
+      case OPEN:
+        if (digitalRead(LOADING_DRUM_MOVER_MOTOR_FRONT_END_PIN)) {
+          MOVE_LOADING_DRUM_FORWARD;
+        } else {
+          STOP_LOADING_DRUM_MOVER;
+        }
+        break;
+
+      case CLOSE:
+        if (digitalRead(LOADING_DRUM_MOVER_MOTOR_BACK_END_PIN)) {
+          MOVE_LOADING_DRUM_BACKWARD;
+        } else {
+          STOP_LOADING_DRUM_MOVER;
+        }
+        break;
+    }
+  }
+
+private:
+  State _state = NONE;
 };
 
 #endif
