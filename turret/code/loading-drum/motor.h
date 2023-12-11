f2f1943fbe7a4cdbf7e52bab5cbd5c2d78535b4e
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-12-11 11:31:40 +0100

    Configure motor

--- turret/code/loading-drum/motor.h
+++ turret/code/loading-drum/motor.h
@@ -8,7 +8,7 @@
 #define CLOSE_GATE \
   digitalWrite(GATE_MOTOR_FORWARD_PIN, HIGH); \
   digitalWrite(GATE_MOTOR_BACKWARD_PIN, LOW)
-  
+
 #define STOP_GATE \
   digitalWrite(GATE_MOTOR_FORWARD_PIN, HIGH); \
   digitalWrite(GATE_MOTOR_BACKWARD_PIN, HIGH)
@@ -28,26 +28,68 @@
   digitalWrite(LOADING_DRUM_MOVER_MOTOR_BACKWARD_PIN, HIGH); \
   digitalWrite(LOADING_DRUM_MOVER_MOTOR_FORWARD_PIN, HIGH)
 
-enum State {
+#define ROTATE_CLOCKWISE_DRUM \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_BACKWARD_PIN, LOW)
+
+#define ROTATE_COUNTER_CLOCKWISE_DRUM \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_FORWARD_PIN, LOW); \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_BACKWARD_PIN, HIGH)
+
+#define STOP_ROTATION \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(LOADING_DRUM_ROTATION_MOTOR_BACKWARD_PIN, HIGH)
+
+#define ROTATION_CHECKPOINT_TRIGGERED analogRead(LOADING_DRUM_ROTATION_MOTOR_POSITION_PIN) < 500
+
+#define PUSH_FEEDER \
+  digitalWrite(FEEDER_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(FEEDER_MOTOR_BACKWARD_PIN, LOW)
+
+#define PULL_FEEDER \
+  digitalWrite(FEEDER_MOTOR_FORWARD_PIN, LOW); \
+  digitalWrite(FEEDER_MOTOR_BACKWARD_PIN, HIGH)
+
+#define STOP_FEEDER \
+  digitalWrite(FEEDER_MOTOR_FORWARD_PIN, HIGH); \
+  digitalWrite(FEEDER_MOTOR_BACKWARD_PIN, HIGH)
+
+#define FEEDER_PUSHED_POSITION_TRIGGERED analogRead(FEEDER_CONSTRAIN_IR_PIN) < 500
+
+#define FEEDER_PULLED_POSITION_TRIGGERED digitalRead(FEEDER_CONSTRAIN_END_PIN)
+
+enum class State {
   NONE,
   OPEN,
   CLOSE
 };
 
+enum class RotationState {
+  NEXT,
+  PREVIOUS,
+  STOP
+};
+
+enum class FeederState {
+  PUSH,
+  PULL,
+  STOP
+};
+
 class Gate {
 public:
   void open() {
-    _state = OPEN;
+    _state = State::OPEN;
   }
 
   void close() {
-    _state = CLOSE;
+    _state = State::CLOSE;
   }
 
   void tick() {
     switch (_state) {
 
-      case OPEN:
+      case State::OPEN:
         if (GATE_OPENED_TRIGGERED) {
           STOP_GATE;
         } else {
@@ -55,7 +97,7 @@ public:
         }
         break;
 
-      case CLOSE:
+      case State::CLOSE:
         if (GATE_CLOSED_TRIGGERED) {
           STOP_GATE;
         } else {
@@ -66,23 +108,23 @@ public:
   }
 
 private:
-  State _state = NONE;
+  State _state = State::NONE;
 };
 
 class LoadingDrumMover {
 public:
   void activate() {
-    _state = OPEN;
+    _state = State::OPEN;
   }
 
   void deactivate() {
-    _state = CLOSE;
+    _state = State::CLOSE;
   }
 
   void tick(void) {
     switch (_state) {
 
-      case OPEN:
+      case State::OPEN:
         if (digitalRead(LOADING_DRUM_MOVER_MOTOR_FRONT_END_PIN)) {
           MOVE_LOADING_DRUM_FORWARD;
         } else {
@@ -90,7 +132,7 @@ public:
         }
         break;
 
-      case CLOSE:
+      case State::CLOSE:
         if (digitalRead(LOADING_DRUM_MOVER_MOTOR_BACK_END_PIN)) {
           MOVE_LOADING_DRUM_BACKWARD;
         } else {
@@ -101,7 +143,84 @@ public:
   }
 
 private:
-  State _state = NONE;
+  State _state = State::NONE;
+};
+
+class DrumRotation {
+public:
+  void next() {
+    _rotation_state = RotationState::NEXT;
+    ROTATE_CLOCKWISE_DRUM;
+    _timestamp = millis();
+  }
+
+  void previous() {
+    _rotation_state = RotationState::PREVIOUS;
+    ROTATE_COUNTER_CLOCKWISE_DRUM;
+    _timestamp = millis();
+  }
+
+  void tick(void) {
+    switch (_rotation_state) {
+
+      case RotationState::NEXT:
+        ROTATE_CLOCKWISE_DRUM;
+        if (millis() - _timestamp > 100 && ROTATION_CHECKPOINT_TRIGGERED) {
+          STOP_ROTATION;
+          _rotation_state = RotationState::STOP;
+        }
+        break;
+        
+      case RotationState::PREVIOUS:
+        ROTATE_COUNTER_CLOCKWISE_DRUM;
+        if (millis() - _timestamp > 100 && ROTATION_CHECKPOINT_TRIGGERED) {
+          STOP_ROTATION;
+          _rotation_state = RotationState::STOP;
+        }
+        break;
+    }
+  }
+private:
+  RotationState _rotation_state = RotationState::STOP;
+  uint32_t _timestamp = 0;
+};
+
+class Feeder {
+  public:
+    void tick(void) {
+      switch(_state) {
+
+        case FeederState::PUSH:
+          PUSH_FEEDER;
+          if (FEEDER_PUSHED_POSITION_TRIGGERED) {
+            STOP_FEEDER;
+            _state = FeederState::STOP;
+          }
+          break;
+
+        case FeederState::PULL:
+          PULL_FEEDER;
+          if (millis() - _timestamp > 700 && FEEDER_PULLED_POSITION_TRIGGERED) {
+            STOP_FEEDER;
+            _state = FeederState::STOP;
+          }
+          break;
+      }
+    }
+
+    void push() {
+      _state = FeederState::PUSH;
+      
+    }
+
+    void pull() {
+      _state = FeederState::PULL;
+      _timestamp = millis();
+    }
+  
+  private:
+    FeederState _state = FeederState::STOP;
+    uint32_t _timestamp = 0;
 };
 
 #endif
