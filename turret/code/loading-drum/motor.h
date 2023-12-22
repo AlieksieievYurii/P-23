288a31f1ff1256de7c33c892ba1c5bf35f58f917
AlieksieievYurii <alieksieiev.yurii@gmail.com>
2023-12-22 10:10:13 +0100

    Add timeouts for the motors in loading-drum

--- turret/code/loading-drum/motor.h
+++ turret/code/loading-drum/motor.h
@@ -58,8 +58,14 @@
 
 #define FEEDER_PULLED_POSITION_TRIGGERED digitalRead(FEEDER_CONSTRAIN_END_PIN)
 
+#define ENABLE_LOADING_DRUM_MOVER_TIMEOUT
+#define ENABLE_GATE_TIMEOUT
+
+#define LOADING_DRUM_MOVER_TIMEOUT 8000
+#define GATE_TIMEOUT 1000
+
 enum class State {
-  NONE,
+  STOP,
   OPEN,
   CLOSE
 };
@@ -80,70 +86,119 @@ class Gate {
 public:
   void open() {
     _state = State::OPEN;
+    _failed = false;
+    _timestamp = millis();
   }
 
   void close() {
     _state = State::CLOSE;
+    _failed = false;
+    _timestamp = millis();
   }
 
   void tick() {
+    if (_failed) {
+      _state = State::STOP;
+    }
+
     switch (_state) {
 
       case State::OPEN:
         if (GATE_OPENED_TRIGGERED) {
-          STOP_GATE;
+          _state = State::STOP;
         } else {
+#ifdef ENABLE_GATE_TIMEOUT
+          if (millis() - _timestamp > GATE_TIMEOUT)
+            _failed = true;
+#endif
           OPEN_GATE;
         }
         break;
 
       case State::CLOSE:
         if (GATE_CLOSED_TRIGGERED) {
-          STOP_GATE;
+          _state = State::STOP;
         } else {
+#ifdef ENABLE_GATE_TIMEOUT
+          if (millis() - _timestamp > GATE_TIMEOUT)
+            _failed = true;
+#endif
           CLOSE_GATE;
         }
+        break;
+
+      case State::STOP:
+        STOP_GATE;
         break;
     }
   }
 
 private:
-  State _state = State::NONE;
+  State _state = State::STOP;
+  bool _failed = false;
+  uint32_t _timestamp = 0;
 };
 
 class LoadingDrumMover {
 public:
   void activate() {
     _state = State::OPEN;
+    _failed = false;
+    _timestamp = millis();
   }
 
   void deactivate() {
     _state = State::CLOSE;
+    _failed = false;
+    _timestamp = millis();
   }
 
   void tick(void) {
-    switch (_state) {
+    if (_failed) {
+      _state = State::STOP;
+    }
 
+    switch (_state) {
       case State::OPEN:
         if (digitalRead(LOADING_DRUM_MOVER_MOTOR_FRONT_END_PIN)) {
+
+#ifdef ENABLE_LOADING_DRUM_MOVER_TIMEOUT
+          if (millis() - _timestamp > LOADING_DRUM_MOVER_TIMEOUT)
+            _failed = true;
+#endif
+
           MOVE_LOADING_DRUM_FORWARD;
+
         } else {
-          STOP_LOADING_DRUM_MOVER;
+          _state = State::STOP;
         }
         break;
 
       case State::CLOSE:
         if (digitalRead(LOADING_DRUM_MOVER_MOTOR_BACK_END_PIN)) {
+
+#ifdef ENABLE_LOADING_DRUM_MOVER_TIMEOUT
+          if (millis() - _timestamp > LOADING_DRUM_MOVER_TIMEOUT)
+            _failed = true;
+#endif
+
           MOVE_LOADING_DRUM_BACKWARD;
+
         } else {
-          STOP_LOADING_DRUM_MOVER;
+          _state = State::STOP;
         }
+        break;
+
+      case State::STOP:
+        STOP_LOADING_DRUM_MOVER;
         break;
     }
   }
 
 private:
-  State _state = State::NONE;
+  State _state = State::STOP;
+  bool _failed = false;
+  uint32_t _timestamp = 0;
 };
 
 class DrumRotation {
@@ -170,7 +225,7 @@ public:
           _rotation_state = RotationState::STOP;
         }
         break;
-        
+
       case RotationState::PREVIOUS:
         ROTATE_COUNTER_CLOCKWISE_DRUM;
         if (millis() - _timestamp > 100 && ROTATION_CHECKPOINT_TRIGGERED) {
@@ -186,41 +241,40 @@ private:
 };
 
 class Feeder {
-  public:
-    void tick(void) {
-      switch(_state) {
+public:
+  void tick(void) {
+    switch (_state) {
 
-        case FeederState::PUSH:
-          PUSH_FEEDER;
-          if (FEEDER_PUSHED_POSITION_TRIGGERED) {
-            STOP_FEEDER;
-            _state = FeederState::STOP;
-          }
-          break;
+      case FeederState::PUSH:
+        PUSH_FEEDER;
+        if (FEEDER_PUSHED_POSITION_TRIGGERED) {
+          STOP_FEEDER;
+          _state = FeederState::STOP;
+        }
+        break;
 
-        case FeederState::PULL:
-          PULL_FEEDER;
-          if (millis() - _timestamp > 700 && FEEDER_PULLED_POSITION_TRIGGERED) {
-            STOP_FEEDER;
-            _state = FeederState::STOP;
-          }
-          break;
-      }
+      case FeederState::PULL:
+        PULL_FEEDER;
+        if (millis() - _timestamp > 700 && FEEDER_PULLED_POSITION_TRIGGERED) {
+          STOP_FEEDER;
+          _state = FeederState::STOP;
+        }
+        break;
     }
+  }
 
-    void push() {
-      _state = FeederState::PUSH;
-      
-    }
+  void push() {
+    _state = FeederState::PUSH;
+  }
 
-    void pull() {
-      _state = FeederState::PULL;
-      _timestamp = millis();
-    }
-  
-  private:
-    FeederState _state = FeederState::STOP;
-    uint32_t _timestamp = 0;
+  void pull() {
+    _state = FeederState::PULL;
+    _timestamp = millis();
+  }
+
+private:
+  FeederState _state = FeederState::STOP;
+  uint32_t _timestamp = 0;
 };
 
 #endif
